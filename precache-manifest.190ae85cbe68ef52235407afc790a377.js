15487b621db3ab2662bd183921c259673718a94b
hayoola <onaolapo.ayo@gmail.com>
2020-06-18 11:06:46 +0400

    Updates

---
+++ precache-manifest.190ae85cbe68ef52235407afc790a377.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec089229be6a9345ab74d02c629dbec8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30025d5df5a31e9a2acd",
+    "url": "/static/css/main.19db2d11.chunk.css"
+  },
+  {
+    "revision": "b4e4bb35471e258e48f9",
+    "url": "/static/js/2.e4734c5e.chunk.js"
+  },
+  {
+    "revision": "9617c35fefb3909e96d4735aba67fb26",
+    "url": "/static/js/2.e4734c5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30025d5df5a31e9a2acd",
+    "url": "/static/js/main.49b6a490.chunk.js"
+  },
+  {
+    "revision": "bdbc5ffb486ef0126a6d",
+    "url": "/static/js/runtime-main.20573be6.js"
+  },
+  {
+    "revision": "527aec5b0d45839a871a43c8acab0bad",
+    "url": "/static/media/Accounts.527aec5b.png"
+  },
+  {
+    "revision": "7065b15d299f906911a833b7d568cdc2",
+    "url": "/static/media/Branch-&-ATM-Locator.7065b15d.png"
+  },
+  {
+    "revision": "f75f74ae5540ed6ebfe27851bf548afd",
+    "url": "/static/media/Credit-card.f75f74ae.png"
+  },
+  {
+    "revision": "7d00939af32b84d74705982704d0668e",
+    "url": "/static/media/Document_Personal-Details-Update.7d00939a.png"
+  },
+  {
+    "revision": "ed5062976cc953a9e33fe0d2e6d480db",
+    "url": "/static/media/Loan-details.ed506297.png"
+  },
+  {
+    "revision": "fe1f1d5b9f29804311f4e5c0811e67ea",
+    "url": "/static/media/Rewards-&-Loyalty.fe1f1d5b.png"
+  },
+  {
+    "revision": "96110f847d53064c07102de9b25cffe8",
+    "url": "/static/media/banner-img.96110f84.png"
+  },
+  {
+    "revision": "c81fe1408281a2e9651894fdfea00e00",
+    "url": "/static/media/home-page-3-logo@2x.c81fe140.png"
+  },
+  {
+    "revision": "03f8c293711bf82189d6d855aacc4cb0",
+    "url": "/static/media/thumbnail-1.03f8c293.png"
+  },
+  {
+    "revision": "689667fd9e8e796139d5f4dcb3af5c32",
+    "url": "/static/media/thumbnail-2.689667fd.png"
+  },
+  {
+    "revision": "5b5a55e46ff972a92375e55108f0fdb5",
+    "url": "/static/media/thumbnail-3.5b5a55e4.png"
+  },
+  {
+    "revision": "a35a801ce0f875a180f1236035e75de1",
+    "url": "/static/media/thumbnail-4.a35a801c.png"
+  },
+  {
+    "revision": "7f5285b93f0b33b693ee3b8824e5ff89",
+    "url": "/static/media/thumbnail-5.7f5285b9.png"
+  },
+  {
+    "revision": "a35c2154b650e9f10b5e14087d3cb505",
+    "url": "/static/media/thumbnail-6.a35c2154.png"
+  }
+]);
